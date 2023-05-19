c6087799fcd7771cbf47ccac4b60e7f353ff3a51
VinlaRose <vinla.files@gmail.com>
2023-05-19 12:13:59 +0530

    commit

--- my-app/src/components/filters/Filter.jsx
+++ my-app/src/components/filters/Filter.jsx
@@ -1,28 +1,23 @@
-import { useContext, useEffect, useReducer, useState } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../../context/DataContext";
 
 
-
 export const Filters = () => {
-    const {sortOrder, state, clearFilter, handleSorting} = useContext(DataContext);
-    
-    
 
-    
-   
-    
+     
     return(
 
         <div>
             <p className="filterHeading">
                 <span className="filterPrice">PRICE</span>
-                <button onClick={clearFilter} className="clear">Clear</button>
+                <button  className="clear">Clear</button>
             </p>
             <div className="sortingPrice">
                 <h3>Price</h3>
-                <div><input type="radio" value="descending" checked={sortOrder === "descending"} onChange={handleSorting} /> High to Low</div>
-                <div><input type="radio" value="ascending" checked={sortOrder === "ascending"} onChange={handleSorting} /> Low to High</div>
-               
+                <div><input type="radio" value="highToLow" checked={sortOrder === "highToLow"}onChange={() => dispatch({type: "sortOrder", payload: "highToLow" })} /> High to Low</div>
+                <div><input type="radio" value="lowToHigh" checked={sortOrder === "lowToHigh"} onChange={() => dispatch({type: "sortOrder", payload: "lowToHigh" })} /> Low to High</div>
+
+
             </div>
         </div>
         
--- my-app/src/context/DataContext.js
+++ my-app/src/context/DataContext.js
@@ -1,6 +1,7 @@
-import React, { createContext, useEffect, useState, useReducer } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const DataContext = createContext();
+
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
@@ -19,51 +20,9 @@ export const DataProvider = ({ children }) => {
     getData();
   }, []);
 
-  const [sortOrder, setSortOrder] = useState();
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "sorting":
-        let sortedState;
-
-        if (action.payload === "descending") {
-          sortedState = [...data].sort((a, b) => b.price - a.price);
-        } else if (action.payload === "ascending") {
-          sortedState = [...data].sort((a, b) => a.price - b.price);
-        } else {
-          sortedState = state;
-        }
-
-        return sortedState;
-    case "clearFilter" :
-        return data
-      default:
-        return state;
-    }
-  };
-
-  const [items, dispatch] = useReducer(reducer, data);
-
-  const handleSorting = (e) => {
-    setSortOrder(e.target.value);
-    dispatch({ type: "sorting", payload: e.target.value });
-  };
-
-  useEffect(() => {
-    dispatch({ type: "sorting", payload: sortOrder });
-  }, [sortOrder]);
-
-  const clearFilter = () => {
-    setSortOrder(" ");
-    dispatch({ type: "clearFilter" });
-  
-  }
-console.log("data", data)
-console.log("items", items)
-  
 
   return (
-    <DataContext.Provider value={{ data, handleSorting, sortOrder, clearFilter }}>
+    <DataContext.Provider value={{ data }}>
       {children}
     </DataContext.Provider>
   );
